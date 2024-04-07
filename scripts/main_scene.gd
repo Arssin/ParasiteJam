19198cab080253bf0161de94fd63ab4fd510e692
Mateusz Binięda <mat.binieda@gmail.com>
2024-04-08 00:43:36 +0200

    init project

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -3,8 +3,26 @@ extends Control
 
 @onready var hud : Control = %HUD
 @onready var menu: Control = %Menu
+@onready var main = %Main
+@onready var camera = %Camera2D
 
-# Called when the node enters the scene tree for the first time.
+var level_instance
+
+
+func unload_level():
+	if (is_instance_valid(level_instance)):
+		level_instance.queue_free()
+	level_instance = null
+
+
+func load_level(level_name: String):
+	unload_level()
+	var level_path := "res://levels/%s.tscn" % level_name
+	var level_resource := load(level_path)
+	if (level_resource):
+		level_instance = level_resource.instance()
+		%Main.add_child(level_instance)
+
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -12,3 +30,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_game_pressed() -> void:
+	load_level("Level1")
