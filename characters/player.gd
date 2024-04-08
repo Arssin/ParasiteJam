06bcf8ea0279b6e4731869d6da21f0e24fcfacda
Mateusz Binięda <mat.binieda@gmail.com>
2024-04-08 17:48:18 +0200

    add player movement

--- characters/player.gd
+++ characters/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var player_speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	move_and_slide()
+
+
+
+func _input(_event: InputEvent) -> void:
+	var input_direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
+	
+	if input_direction:
+		print('clicked', input_direction)
+		velocity = input_direction * player_speed
+	else:
+			velocity = input_direction * 0
+	
+	
