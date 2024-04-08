5183cbe15942090727d2b8a6c25c9fa01afc8495
Mateusz Binięda <mat.binieda@gmail.com>
2024-04-08 19:08:13 +0200

    player shooting himself

--- characters/player.gd
+++ characters/player.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 class_name Player
 
 var player_speed = 100
-
+var mouse_clicked = false
 var projectile := preload("res://characters/player_projectile.tscn")
+var shooted_pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,35 +14,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var position_mouse_x = get_global_mouse_position().x
-	
-	if Input.is_action_just_pressed("left_mouse"):
-		shoot()
+	if mouse_clicked:
+		velocity = shooted_pos * player_speed
 	
 	move_and_slide()
 
-func shoot():
-	var mouse_position = get_global_mouse_position()
-	var direction = (mouse_position - global_position).normalized()
-	velocity = direction * player_speed
 
-	#while true:
-		#var move_and_collide_result = move_and_collide(velocity)
-		#if move_and_collide_result:
-			#if move_and_collide_result.collider:
-				#break
-	#var bullet = projectile.instantiate()
-	#bullet.position = $".".global_position
-	#bullet.direction = global_position.direction_to(get_global_mouse_position())
-	#add_child(bullet)
+
 
 func _input(_event: InputEvent) -> void:
 	var input_direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
 	
-	if input_direction:
-		velocity = input_direction * player_speed
-	else:
-		velocity = input_direction * 0
+	if !mouse_clicked:
+		if Input.is_action_just_pressed("left_mouse"):
+			var mouse_position = get_global_mouse_position()
+			shooted_pos = (mouse_position - global_position).normalized()
+			mouse_clicked = true
+
+	if !mouse_clicked:
+		if input_direction:
+			velocity = input_direction * player_speed
+		else:
+			velocity = input_direction * 0
 	
 func teleport(position):
 	global_position = position
