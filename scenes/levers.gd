3af2b0049e909a55c9b02898fc9658e7c314e72a
Mateusz Binięda <mat.binieda@gmail.com>
2024-04-10 23:20:46 +0200

    add working lever

--- scenes/levers.gd
+++ scenes/levers.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var canInteract = false
+var isSwitched = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		canInteract = true
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		canInteract = false
+
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact") && canInteract:
+		if !isSwitched:
+			isSwitched = true
+			$AnimatedSprite2D.play("switch_to_normal")
+		else:
+			isSwitched = false
+			$AnimatedSprite2D.play("switch_back")
