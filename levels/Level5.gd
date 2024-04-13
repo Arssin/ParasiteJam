dacf3b79bae7da4f3e90ffcd2401228f27c8b367
Mateusz Binięda <mat.binieda@gmail.com>
2024-04-13 04:45:59 +0200

    level 6

--- levels/Level5.gd
+++ levels/Level5.gd
@@ -9,3 +9,22 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		Global.load_level("Level6")
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.die()
+
+
+func _on_levers_used_lever(value: Variant) -> void:
+	if value:
+		$doors.anim.play("open")
+		$doors.collision.disabled = true
+	else:
+		$doors.anim.play("close")
+		$doors.collision.disabled = false
