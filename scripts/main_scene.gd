ee22029c94606bd81aa954cbeb5f9aea19eadff1
Mateusz Binięda <mat.binieda@gmail.com>
2024-04-13 19:51:03 +0200

    sound off and in

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -6,10 +6,18 @@ extends Control
 @onready var main = %Main
 @onready var camera = %Camera2D
 
+@onready var music_off = load("res://assets/music off.png")
+@onready var music_on = load("res://assets/music on.png")
+
+var isMusicOn = true
+
 var level_instance
 
 func _ready() -> void:
-	pass
+	if isMusicOn:
+		%"Music Button".icon = music_on
+	else:
+		%"Music Button".icon = music_off
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -38,3 +46,18 @@ func _on_button_pressed() -> void:
 	%Lost.visible = false
 	%Menu.visible = true
 	#get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+	
+
+
+
+func _on_music_button_pressed() -> void:
+	if isMusicOn:
+		isMusicOn = false
+		%"Music Button".icon = music_off
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+	else: 
+		isMusicOn = true
+		%"Music Button".icon = music_on
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sfx"), false)
